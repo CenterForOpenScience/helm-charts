fa3d913618fbfcfc5e8fe4b4411605b4470b9ba5
icereval <icereval@gmail.com>
2018-10-12 09:50:54 -0400

    standardize osf es chart w/ same version and warning regarding helm package bug

--- elasticsearch6/copy.sh
+++
@@ -1,9 +0,0 @@
-###################################################################################
-####### THIS CHART IS A COPY OF ELASTICSEARCH (DO NOT MODIFY)               #######
-####### DUE TO A BUG IN HELM PUBLISH WHEN USING ALIAS AND MULTIPLE VERSIONS #######
-####### SEE: https://github.com/helm/helm/issues/3909                       #######
-###################################################################################
-
-rm -rf ./templates
-cp -r ../elasticsearch/ .
-sed -i '' -e "s|^name: elasticsearch|name: elasticsearch6|" Chart.yaml
